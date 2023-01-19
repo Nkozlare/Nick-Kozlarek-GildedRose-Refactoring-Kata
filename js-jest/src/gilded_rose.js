cb5e94d3b94d2a23b9755b4c578ffbceafa55e13
Nicholas Kozlarek <nick@Nicholass-MacBook-Pro.local>
2023-01-19 14:54:24 -0700

    Refactor gilded rose code to use switch and case rather than nested if's

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -12,51 +12,35 @@ class Shop {
   }
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
+      // variables for shortening code.
+      let name = this.items[i].name;
+      let quality = this.items[i].quality;
+      let sellIn = this.items[i].sellIn;
+      switch (name) {
+        case 'Aged Brie': 
+          quality < 50 ? this.items[i].quality ++ : null;
+          break;
+        case 'Backstage passes to a TAFKAL80ETC concert':
+          sellIn > 10 ? this.items[i].quality ++ :
+          sellIn < 11 && sellIn > 5 ? this.items[i].quality += 2 :
+          sellIn < 6 && sellIn >= 0 ? this.items[i].quality += 3 :
+          sellIn < 0 ? this.items[i].quality = 0 : null;
+          this.items[i].quality > 50 ? this.items[i].quality = 50 : null;
+          break;
+        case 'Sulfuras, Hand of Ragnaros':
+          break;
+        case 'Conjured':
+          sellIn >= 0 ? this.items[i].quality -= 2 :
+          sellIn < 0 ? this.items[i].quality -= 4 : null;
+          this.items[i].quality < 0 ? this.items[i].quality = 0 : null;
+          break;
+        default :
+          sellIn >= 0 ? this.items[i].quality -= 1 :
+          sellIn < 0 ? this.items[i].quality -= 2 : null;
+          this.items[i].quality < 0 ? this.items[i].quality = 0 : null;
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
-      }
+      name !== 'Sulfuras, Hand of Ragnaros' ? this.items[i].sellIn -- : null;
     }
-
     return this.items;
   }
 }
